ed38dc8d6ec0d7005c92565f5a0f306969ce0ab8
mikhael z <mikhael928@gmail.com>
2023-11-13 10:21:31 -0300

    Starting the UI development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateComponent } from './components/thinkings/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
